4ad866a3675cd756435e820e235a204a4442cc1c
Alexander Koskovich <zvnexus@outlook.com>
2020-10-20 14:32:27 -0700

    ipacm: Remove board/arch checks on IPACM_DATA.
     * Legacy targets wouldn't be picking up all the split makefiles
       like CAF does, so you'd have to be intentionally including this.
    
    Change-Id: Iaeb4f40f548f53bfc5a661197f315391873d5420

--- ipacm_vendor_product.mk
+++ ipacm_vendor_product.mk
@@ -37,13 +37,6 @@ BOARD_IPAv3_LIST += bengal
 BOARD_ETH_BRIDGE_LIST := msmnile
 BOARD_ETH_BRIDGE_LIST += kona
 
-ifneq ($(call is-board-platform-in-list,$(BOARD_PLATFORM_LIST)),true)
-ifneq (,$(filter $(QCOM_BOARD_PLATFORMS),$(TARGET_BOARD_PLATFORM)))
-ifneq (, $(filter aarch64 arm arm64, $(TARGET_ARCH)))
-
 PRODUCT_PACKAGES += $(IPACM_DATA)
 
-endif # $(TARGET_ARCH)
-endif
-endif
 endif
